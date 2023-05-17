23e0e1d0a31b3989afae18d4b8e65631abccf3a8
Weigandt Timoteo <weigandttimoteo@gmail.com>
2023-05-17 18:50:22 -0300

    Se añade administrador de archivos y una nueva libreria al requirements.txt

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
 from pynput import keyboard
+from tkinter import filedialog
+import os
+import shutil
+
+def add_file():
+    print("add file")
+    file = filedialog.askopenfile(mode='r', filetypes=[('MP3', '*.mp3')])
+    if file:
+        filepath = os.path.abspath(file.name)
+        print(filepath)
+        shutil.copy(filepath, "./Audios")
+        print("archivo añadido a la carpeta Audios")
+
+def list_files():
+    print("list files")
+    files = os.listdir("./Audios")
+    print("archivos en la carpeta Audios:")
+    print(files)
+    
 
 print("Iniciando...")
+opcion = input("Seleccione una opcion:\n1. Agregar archivo\n2. Ver archivos\n3. Salir\n")
+if opcion == "1":
+    add_file()
+elif opcion == "2":
+    list_files()
+elif opcion == "3":
+    print("Saliendo...")
+    exit()
